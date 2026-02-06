e1ec491b15d4d29788dd3ca3599c3d6649222e9a
Tamim189 <tamimr4@nycstudents.net>
2026-02-06 09:24:00 -0500

    filter by text search

--- HTML5 Domain 5 Project/HTML5 Domain 5 Project/script.js
+++ HTML5 Domain 5 Project/HTML5 Domain 5 Project/script.js
@@ -1,49 +1,88 @@
-console.log("js console");
+console.log("js started");
+
+var data;
+var grid = document.querySelector(".grid-container");
+
+if (localStorage.getItem("datalist")) {
+  data = JSON.parse(localStorage.getItem("datalist"));
+  console.log("Loaded from localStorage");
+  makeCards();
+} else {
+  var xhttp = new XMLHttpRequest();
+
+  xhttp.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+      data = JSON.parse(this.responseText);
+      console.log("Loaded from moviesdata.JSON");
+
+      localStorage.setItem("datalist", JSON.stringify(data));
+      console.log("Saved starter movies to localStorage");
+
+      makeCards();
+    }
+  };
+
+  xhttp.open("GET", "moviesdata.JSON", true);
+  xhttp.send();
+}
+
+// RENDER MOVIE CARDS
+function makeCards() {
+  grid.innerHTML = "";
+
+  data.forEach(function (movie) {
+    let card = document.createElement("div");
+    card.classList.add("card");
+
+    let textData =
+      "<div class='movie-title'>" + movie.title + "</div>" +
+      "<div>Cast: " + movie.cast + "</div>" +
+      "<div>Release Date: " + movie.year + "</div>";
+
+    card.innerHTML = textData;
+
+    if (movie.imgSrc) {
+      card.style.backgroundImage = "url('" + movie.imgSrc + "')";
+      card.style.backgroundSize = "cover";
+      card.style.backgroundPosition = "center";
+    }
+
+    grid.appendChild(card);
+  });
+
+  console.log("cards refreshed");
+}
 
 
-let data;
-let grid = document.querySelector(".grid-container");
+// filter search
+var search = document.getElementById("search");
 
 
-var xhttp = new XMLHttpRequest();
 
+search.addEventListener("keyup", function () {
+  var searchText = search.value.toLowerCase();
 
-xhttp.onreadystatechange = function(){
-   if(this.readyState == 4 && this.status == 200){
+  var filteredData = data.filter(function (movie) {
+    return movie.title.toLowerCase().includes(searchText);
+  });
 
+  grid.innerHTML = "";
 
-       data = JSON.parse(xhttp.responseText);
-       console.log(data);
+  filteredData.forEach(function (movie) {
+    let card = document.createElement("div");
+    card.classList.add("card");
 
+    card.innerHTML =
+      "<div class='movie-title'>" + movie.title + "</div>" +
+      "<div>Cast: " + movie.cast + "</div>" +
+      "<div>Release Date: " + movie.year + "</div>";
 
-       data.forEach(function(movie){
-           let card = document.createElement("div");
-           card.classList.add("card");
+    if (movie.imgSrc) {
+      card.style.backgroundImage = "url('" + movie.imgSrc + "')";
+      card.style.backgroundSize = "cover";
+      card.style.backgroundPosition = "center";
+    }
 
-
-           let textData =
-           "<div class = 'movie-title'>" + movie.title + "</div>" +
-           "<span>" +
-           "Cast: "  + movie.cast + "<br>" +
-           "Release Date: " + movie.year + "<br>" +
-           "Needs Research: " +
-           "</span>";
-
-
-           card.innerHTML = textData;
-
-
-           if(movie.imgSrc){
-               card.style.backgroundImage = "url('" + movie.imgSrc +"')";
-           }
-
-
-           grid.appendChild(card);
-       });
-   }
-};
-
-
-xhttp.open("GET", "moviedata.json", true);
-xhttp.send();
-
+    grid.appendChild(card);
+  });
+});
